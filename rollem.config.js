a5b0da5a5df3b2d6e7518f51d717558a46e45c9b
Lajos.Meszaros <m_lajos@hotmail.com>
2017-05-14 18:12:06 +0200

    feat(calculation): Added distributeText and ramda
    Added distributeText() which can distribute a height into smaller chunks based on a set of container
    sizes

--- rollem.config.js
+++ rollem.config.js
@@ -1,5 +1,8 @@
 import uglify from 'rollup-plugin-uglify'
 import buble from 'rollup-plugin-buble'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import ramda from 'rollup-plugin-ramda'
 
 export default [{
   entry: 'src/index.js',
@@ -8,6 +11,16 @@ export default [{
   moduleName: 'SplitArticle',
   sourceMap: true,
   plugins: [
+    resolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs({
+      namedExports : {
+        'node_modules/ramda/index.js': Object.keys(require('ramda'))
+      }
+    }),
+    ramda(),
     buble(),
     uglify()
   ]
--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
+import {
+  always,
+  apply,
+  compose,
+  curryN,
+  gt,
+  ifElse,
+  inc,
+  length,
+  map,
+  modulo,
+  multiply,
+  repeat,
+  sum,
+  zip
+} from 'ramda'
 import onReady from './helpers/onready'
+
+const sumCounts = curryN(2, compose(sum, map(apply(multiply)), zip))
+const generateCounts = compose(repeat(0), length)
+
+const distributeText = (textSize, containerSizes, counts = generateCounts(containerSizes), index = 0) => {
+  counts[index]++
+
+  return ifElse(
+    compose(gt(textSize), () => sumCounts(containerSizes, counts)),
+    () => distributeText(textSize, containerSizes, counts, modulo(inc(index), length(containerSizes))),
+    always(counts)
+  )()
+}
 
 class SplitArticle {
   constructor (config) {
-    // 1) measure the available space in the target
-
     onReady(() => {
+      const textSize = 1753 // height of the text, that we want to split up
+      const containerSizes = [300, 50, 300, 75] // heights of every target container
+
+      console.log(distributeText(textSize, containerSizes)) // [3, 3, 2, 2]
+
       window.addEventListener('resize', () => {
         console.log('Window resized, need to recalculate stuff')
       })

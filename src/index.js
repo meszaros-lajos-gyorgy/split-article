62b53c50a25a4f898bb306b1b1892427c13da865
Lajos Meszaros <lajos.meszaros@performgroup.com>
2017-05-16 18:41:02 +0200

    feat(measurement): Added column width measuring and sliced 1st paragraph into words for POC
    Sliced first paragraph into valid HTML fragmants, which will be joined back into lines

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-import {
-  always,
-  apply,
-  compose,
-  curryN,
-  gt,
-  ifElse,
-  inc,
-  length,
-  map,
-  modulo,
-  multiply,
-  repeat,
-  sum,
-  zip
-} from 'ramda'
 import onReady from './helpers/onready'
+import {
+  // always,
+  // apply,
+  clone,
+  compose,
+  // curryN,
+  // gt,
+  // ifElse,
+  // inc,
+  join,
+  // length,
+  // map,
+  merge,
+  // modulo,
+  // multiply,
+  // prop,
+  repeat
+  // sum,
+  // zip
+} from 'ramda'
 
+/*
 const sumCounts = curryN(2, compose(sum, map(apply(multiply)), zip))
 const generateCounts = compose(repeat(0), length)
 
@@ -28,19 +33,74 @@ const distributeText = (textSize, containerSizes, counts = generateCounts(contai
     always(counts)
   )()
 }
+*/
+
+const generateMeasureText = compose(join(''), repeat('a'))
+
+const getMeasuredWidth = (source, width) => {
+  const measure = document.createElement('div')
+  measure.textContent = generateMeasureText(width)
+  measure.style = 'position:absolute;visibility:hidden'
+
+  let result
+
+  source.appendChild(measure)
+  result = measure.scrollWidth
+  source.removeChild(measure)
+
+  return result
+}
 
 class SplitArticle {
-  constructor (config) {
+  constructor (rawConfig) {
+    this.config = merge({ width: 50 }, rawConfig)
+
     onReady(() => {
+      /*
       const textSize = 1753 // height of the text, that we want to split up
       const containerSizes = [300, 50, 300, 75] // heights of every target container
 
       console.log(distributeText(textSize, containerSizes)) // [3, 3, 2, 2]
+      */
 
-      window.addEventListener('resize', () => {
-        console.log('Window resized, need to recalculate stuff')
+      this.children = Array.from(this.config.source.children)
+      this.config.source.style = 'height:0;position:absolute;overflow:hidden'
+
+      this.resizeSource()
+
+      const elements = this.children[0].innerHTML.match(/(?:<[^>]+>|\S+)/g)
+      const tags = []
+      const words = []
+
+      elements.forEach(element => {
+        if (element.match(/^<\//) !== null) {
+          tags.pop()
+        } else if (element.match(/^</) !== null) {
+          tags.push(element)
+        } else {
+          words.push([
+            clone(tags),
+            element,
+            clone(tags.reverse().map(tag => '</' + tag.replace(/^<(\w+).+$/, '$1') + '>'))
+          ])
+        }
       })
+
+      // console.log(words);
+
+      // console.log(map(prop('scrollHeight'))(this.children))
+
+      /*
+      window.addEventListener('resize', () => {
+
+      })
+      */
     })
+  }
+
+  resizeSource () {
+    this.measuredWidth = getMeasuredWidth(this.config.source, this.config.width)
+    this.config.source.style.width = this.measuredWidth + 'px'
   }
 }
 

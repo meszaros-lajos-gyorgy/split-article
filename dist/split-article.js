1a3c44a82760e2151de635e14cf4f932e58ab0c1
Lajos Meszaros <lajos.meszaros@performgroup.com>
2017-05-24 14:05:14 +0200

    chore(package): Added workspace:save and workspace:restore commands
    This is esentially a remote version of git stash, which saves all changes to a given branch

--- dist/split-article.js
+++ dist/split-article.js
@@ -1,4 +1,4 @@
-// split-article - created by Lajos Meszaros <m_lajos@hotmail.com> - MIT licence - last built on 2017-05-23
+// split-article - created by Lajos Meszaros <m_lajos@hotmail.com> - MIT licence - last built on 2017-05-24
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :

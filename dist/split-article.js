900aef829b38ad4be17a178d5eb6f7454d43bcb7
Lajos Meszaros <lajos.meszaros@performgroup.com>
2017-06-20 19:17:08 +0200

    build(dist): Changed dist staging code a bit
    Removed unnecessary switch

--- dist/split-article.js
+++ dist/split-article.js
@@ -1,4 +1,4 @@
-// split-article - created by Lajos Meszaros <m_lajos@hotmail.com> - MIT licence - last built on 2017-06-19
+// split-article - created by Lajos Meszaros <m_lajos@hotmail.com> - MIT licence - last built on 2017-06-20
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
